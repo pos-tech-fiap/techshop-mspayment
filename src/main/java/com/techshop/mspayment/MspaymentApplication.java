555af3313c90deb4e9ea115006edd89f5d90c950
Bruno Miguel Morais <bm.morais21@gmail.com>
2024-03-21 20:36:45 -0300

    Criando projeto

---
+++ src/main/java/com/techshop/mspayment/MspaymentApplication.java
@@ -0,0 +1,13 @@
+package com.techshop.mspayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MspaymentApplication.class, args);
+	}
+
+}
