1d438b04a2c62918eb54c273b2385ab144a98dca
Thcamposmartins <thcampos_@live.com>
2024-03-23 15:52:41 -0300

    Ajustando mapeamento de portas

--- init.sql
+++ init.sql
@@ -8,6 +8,6 @@ CREATE TABLE tb_payment (
     id BIGINT PRIMARY KEY,
     status VARCHAR(255) not null,
     method VARCHAR(255) not null,
-    userId BIGINT not null,
+    user_id BIGINT not null,
     value double precision not null
 );
