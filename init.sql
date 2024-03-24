e7e4549e82de237acb607cfa0174f03735fe6c5d
Bruno Miguel Morais <bm.morais21@gmail.com>
2024-03-23 23:01:15 -0300

    Arrumando conexão com o banco e transformando id em serial

--- init.sql
+++ init.sql
@@ -1,11 +1,11 @@
 -- Criar database
--- CREATE DATABASE techshop;
+-- CREATE DATABASE mspayment;
 
 -- Conectar ao database
-\c techshop;
+\c mspayment;
 
 CREATE TABLE tb_payment (
-    id BIGINT PRIMARY KEY,
+    id serial PRIMARY KEY,
     status VARCHAR(255) not null,
     method VARCHAR(255) not null,
     user_id BIGINT not null,
